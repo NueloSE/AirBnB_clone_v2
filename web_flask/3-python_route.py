473c8b609429f9190cf80b91a0e39199961ffe9a
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-27 14:08:08 +0100

    feat: out url content

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def rootPage():
+    """
+    Prints root page
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnbPage():
+    """
+    prints hbnbpage
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def nextPage(text):
+    """
+    print a custom page from url
+    """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python")
+@app.route("/python/")
+@app.route("/python/<text>")
+def pythonPage(text='is cool'):
+    """
+    print pythonPage
+    """
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
