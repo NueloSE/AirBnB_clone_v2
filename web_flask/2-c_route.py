32288652ea7c830cde40188f13da55eebac28746
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-27 13:20:11 +0100

    chore: make program comply with pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,24 +3,36 @@
 A script that starts a Flask web application
 """
 
+
 from flask import Flask
 from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def rootPage():
-	return "Hello HBNB!"
+    """
+    Prints root page
+    """
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hbnbPage():
-	return "HBNB"
+    """
+    prints hbnbpage
+    """
+    return "HBNB"
+
 
 @app.route("/c/<text>")
 def nextPage(text):
-	return f"C {escape(text)}"
+    """
+    print a custom page from url
+    """
+    return f"C {escape(text.replace("_", " "))}"
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
